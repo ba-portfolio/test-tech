315213ff3b073dd3b41798744cddab007146bf3d
ba-portfolio <265583137+ba-portfolio@users.noreply.github.com>
2025-07-09 00:55:45 +0200

    chore: back retrait import inutilisés

--- back/src/main/java/com/producttrialmaster/back/security/JwtAuthenticationFilter.java
+++ back/src/main/java/com/producttrialmaster/back/security/JwtAuthenticationFilter.java
@@ -5,15 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.producttrialmaster.back.exception.ApiException;
 
 import io.jsonwebtoken.ExpiredJwtException;
 
