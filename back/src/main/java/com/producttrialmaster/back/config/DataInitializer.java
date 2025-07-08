cc24d29a73ad40a857f36da0a7cd6c2a53bf6a6e
ba-portfolio <265583137+ba-portfolio@users.noreply.github.com>
2025-07-08 02:39:37 +0200

    chore: back ajout initialisation pour accounts ( un admin et un user )

--- back/src/main/java/com/producttrialmaster/back/config/DataInitializer.java
+++ back/src/main/java/com/producttrialmaster/back/config/DataInitializer.java
@@ -2,10 +2,12 @@ package com.producttrialmaster.back.config;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.producttrialmaster.back.dto.AccountCreationDTO;
 import com.producttrialmaster.back.dto.ProductImportDTO;
 import com.producttrialmaster.back.mapper.ProductMapper;
 import com.producttrialmaster.back.model.Product;
 import com.producttrialmaster.back.repository.ProductRepository;
+import com.producttrialmaster.back.service.AccountService;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -34,10 +36,35 @@ public class DataInitializer {
                         .collect(Collectors.toList());
 
                 productRepository.saveAll(products);
-                System.out.println("✅ Produits chargés : " + products.size());
+                System.out.println("Produits chargés : " + products.size());
             } else {
-                System.out.println("❌ Fichier JSON non trouvé.");
+                System.out.println("Fichier JSON non trouvé.");
             }
         };
     }
+
+    @Bean
+    CommandLineRunner loadAccounts(AccountService accountService) {
+        return args -> {
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<List<AccountCreationDTO>> typeRef = new TypeReference<>() {};
+            InputStream inputStream = getClass().getResourceAsStream("/data/accounts.json");
+
+            if (inputStream != null) {
+                List<AccountCreationDTO> accounts = mapper.readValue(inputStream, typeRef);
+
+                for (AccountCreationDTO dto : accounts) {
+                    try {
+                        accountService.createAccount(dto);
+                    } catch (Exception e) {
+                        System.out.println("Erreur lors de la création de l'account : " + dto.getEmail());
+                    }
+                }
+                System.out.println("Comptes chargés : " + accounts.size());
+            } else {
+                System.out.println("Fichier accounts.json non trouvé.");
+            }
+        };
+    }
+
 }
